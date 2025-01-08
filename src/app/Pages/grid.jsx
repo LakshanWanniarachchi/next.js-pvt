35df7cf1d250bd7c6f0bdcc532cc9782946b1794
AkashChathumal69 <akashchathumal470630@gmail.com>
2025-01-08 21:20:36 +0530

    missing letter

--- src/app/Pages/grid.jsx
+++ src/app/Pages/grid.jsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-gray-100 p-4">Component 1</div>
+        <div className="bg-gray-100 p-4">Compoent 1</div>
         <div className="bg-gray-100 p-4">Component 2</div>
         <div className="bg-gray-100 p-4">Component 3</div>
       </div>
